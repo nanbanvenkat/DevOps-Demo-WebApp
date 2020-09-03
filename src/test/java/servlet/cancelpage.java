810cf305c85b8207f35d6704fd4f9f8f854e1557
nanbanvenkat <70356603+nanbanvenkat@users.noreply.github.com>
2020-09-03 14:54:21 -0400

    Update cancelpage.java
    Adding port

--- src/test/java/servlet/cancelpage.java
+++ src/test/java/servlet/cancelpage.java
@@ -17,7 +17,7 @@ public class cancelpage extends HttpServlet {
 				try
 				{
 					Class.forName("org.postgresql.Driver");
-					String jdbcUrl="jdbc:postgresql://34.75.52.135//postgres";
+					String jdbcUrl="jdbc:postgresql://34.75.52.135:5432//postgres";
 					String username="postgres";
 					String password="password";
 					
