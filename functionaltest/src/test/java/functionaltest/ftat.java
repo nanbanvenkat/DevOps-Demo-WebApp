1c21b41c1a8cb7cbb2e3a66504cbae5685ea533c
Umamahesh-DevOpsPjt <70608027+Umamahesh-DevOpsPjt@users.noreply.github.com>
2020-09-15 16:49:07 -0400

    Update ftat.java

--- functionaltest/src/test/java/functionaltest/ftat.java
+++ functionaltest/src/test/java/functionaltest/ftat.java
@@ -3,7 +3,7 @@ package functionaltest;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 //import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
